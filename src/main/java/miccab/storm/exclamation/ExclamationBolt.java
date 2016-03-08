efce40b3c4feee242169aa2e1f1e1572bd9f37e7
Michal <michal@world.com>
2016-03-08 18:42:48 +0100

    first spot and bolt

---
+++ src/main/java/miccab/storm/exclamation/ExclamationBolt.java
@@ -0,0 +1,31 @@
+package miccab.storm.exclamation;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+import java.util.Map;
+
+/**
+ * Created by michal on 08.03.16.
+ */
+public class ExclamationBolt extends BaseRichBolt {
+    private OutputCollector outputCollector;
+
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+        this.outputCollector = outputCollector;
+    }
+
+    public void execute(Tuple tuple) {
+        outputCollector.emit(tuple, new Values(tuple.getString(0) + "!!!"));
+        outputCollector.ack(tuple);
+    }
+
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        outputFieldsDeclarer.declare(new Fields("word"));
+    }
+}
---
+++ src/main/java/miccab/storm/exclamation/ExclamationTopology.java
@@ -0,0 +1,30 @@
+package miccab.storm.exclamation;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.testing.TestWordSpout;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.utils.Utils;
+
+/**
+ * Created by michal on 08.03.16.
+ */
+public class ExclamationTopology {
+
+    public static void main(String [] args) {
+        TopologyBuilder builder = new TopologyBuilder();
+
+        builder.setSpout("word", new TestWordSpout(), 10);
+        builder.setBolt("exclaim1", new ExclamationBolt(), 3).shuffleGrouping("word");
+        builder.setBolt("exclaim2", new ExclamationBolt(), 2).shuffleGrouping("exclaim1");
+
+        Config conf = new Config();
+        conf.setDebug(true);
+
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("test", conf, builder.createTopology());
+        Utils.sleep(4000);
+        cluster.killTopology("test");
+        cluster.shutdown();
+    }
+}
